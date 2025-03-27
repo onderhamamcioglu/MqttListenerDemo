85e6b6a6f05efe4be26a520b465bac06c66bb86b
Önder <eduhamoen001@fh-kaernten.ac.at>
2025-03-27 10:51:37 +0100

    Initial commit

---
+++ src/main/java/com/example/mqttlistenerdemo/MqttListenerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqttlistenerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttListenerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttListenerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mqttlistenerdemo/MqttListenerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqttlistenerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttListenerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
